1631991524ab96843e446fe627576b69abe02999
Yaxin Cheng <yx299612@dal.ca>
2016-09-21 18:13:13 -0300

    Initial Commit

---
+++ SocketKit/SocketKit.h
@@ -0,0 +1,19 @@
+//
+//  SocketKit.h
+//  SocketKit
+//
+//  Created by Yaxin Cheng on 2016-09-21.
+//  Copyright © 2016 Yaxin Cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SocketKit.
+FOUNDATION_EXPORT double SocketKitVersionNumber;
+
+//! Project version string for SocketKit.
+FOUNDATION_EXPORT const unsigned char SocketKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocketKit/PublicHeader.h>
+
+
